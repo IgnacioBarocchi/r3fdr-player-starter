5f9ac9b33cfaf62cd5e732a0f38999262158d8f0
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2023-07-22 18:35:11 +0300

    snap player to ground

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cow-simulator
-https://incandescent-lolly-1ebcb8.netlify.app/# r3fdr-player-starter
+# r3fdr-player-starter
+[https://incandescent-lolly-1ebcb8.netlify.app/# r3fdr-player-starter](https://visionary-salmiakki-846c3a.netlify.app/)
--- src/components/Entities/Robot/index.tsx
+++ src/components/Entities/Robot/index.tsx
@@ -75,7 +75,7 @@ const Robot: FC<{ useOrbitControls: boolean }> = ({ useOrbitControls }) => {
 
   return (
     <RigidBody lockRotations={true} colliders={false} ref={cowBody}>
-      <Bounding args={[0.2, 0.6]} position={[0, 0.5, 0.2]} />
+      <Bounding args={[0.2, 0.6]} position={[0, 0.8, 0.2]} />
       <Sensor args={[0.2, 2]} position={[0, 0.5, 0]} sensor />
       <Robot3DModel state={machineState.value} />
       {[RobotMachineStates.punch, RobotMachineStates.shoot].includes(
