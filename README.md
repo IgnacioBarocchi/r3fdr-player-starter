7c37246cd3c0521585a7b5eb2b0cdd25cdd98b45
Ignacio Barocchi <barocchi.ignacio@gmail.com>
2024-02-08 22:23:36 +0200

    set up monorepo structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# r3fdr-player-starter
-[https://incandescent-lolly-1ebcb8.netlify.app/# r3fdr-player-starter](https://visionary-salmiakki-846c3a.netlify.app/)
--- src/components/Scenario/@types/Scenario.ts
+++
@@ -1,7 +0,0 @@
-export const Scenarios = {
-  ROBOT_PEN: "ROBOT_PEN",
-  MILKING_PARLOR: "MILKING_PARLOR",
-  ANIMAL_TRANSPORT_TRAILER: "ANIMAL_TRANSPORT_TRAILER",
-} as const;
-
-export type Scenario = keyof typeof Scenarios;
